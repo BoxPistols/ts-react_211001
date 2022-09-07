5a1519583f15c17edb8aef59a9201118a9b58c85
aito <BoxPistols@users.noreply.github.com>
2022-09-07 22:04:37 +0900

    add Home

---
+++ src/Home.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+// eslint-disable-next-line import/prefer-default-export
+export const Home: FC = () => (
+  <>
+    <section>
+      <h2>Home</h2>
+      <p>paragraph</p>
+    </section>
+  </>
+)
